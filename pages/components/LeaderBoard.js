ae9391702f95fd3f72a5a0211b9077971e6a423a
Manohar Lakkoju <manoharvirat@gmail.com>
2021-04-15 18:33:45 +0530

    typo

--- pages/components/LeaderBoard.js
+++ pages/components/LeaderBoard.js
@@ -146,7 +146,7 @@ function getParticipants() {
         { name: "Eoin Morgan", points: 0 },
         { name: "Yuzvendra Chahal", points: 0 },
         { name: "Rahul Chahar", points: 0 },
-        { name: "Ben Stokes", points: 0 },
+        { name: "Chris Gayle", points: -12.5 },
         { name: "Riley Meredith", points: 0 },
         { name: "Kartik Tyagi", points: 0 },
       ],
